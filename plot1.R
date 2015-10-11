930d512433e6bc808c581167d1b4884498d53648
Diaa Kasem <me@diaa.me>
2015-10-11 23:38:56 +0200

    Adding plot2 and updating plot1

--- plot1.R
+++ plot1.R
@@ -1,14 +1,12 @@
 # Read the csv data file
 class = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
 df = read.csv(file = "household_power_consumption.txt", sep=";", colClass=class, na.strings="?")
-df$Date <- as.Date(df$Date, "%d/%m/%Y")
-subsetDates <- as.Date(c("2007/02/01", "2007/02/02"), "%Y/%m/%d")
+dateFormat <- "%d/%m/%Y"
+df$Date <- as.Date(df$Date, dateFormat)
+subsetDates <- as.Date(c("01/02/2007", "02/02/2007"), dateFormat)
 df <- subset(df, Date %in% subsetDates)
 
 # Plotting the histogram
-yrange <- range(df$Global_active_power[!is.na(df$Global_active_power)])
-ticks <- pretty(yrange)
-labels <- format(ticks, big.mark=",", scientific=FALSE)
 hist(df$Global_active_power, col='red', xlab="Global Active Power (killowatts)", main="Global Active Power")
 
 
--- plot2.R
+++ plot2.R
@@ -1,19 +1,17 @@
 # Read the csv data file
 class = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
 df = read.csv(file = "household_power_consumption.txt", sep=";", colClass=class, na.strings="?")
-df$DateTime <- as.Date(paste(df$Date, " ", df$Time), "%d/%m/%Y %H:%M")
-subsetDates <- as.Date(c("2007/02/01", "2007/02/02"), "%Y/%m/%d")
+dateFormat <- "%d/%m/%Y"
+df$DateTime <- strptime(paste(df$Date, df$Time), paste(dateFormat, "%H:%M:%S"))
+df$Date <- as.Date(df$Date, dateFormat)
+subsetDates <- as.Date(c("01/02/2007", "02/02/2007"), dateFormat)
 df <- subset(df, Date %in% subsetDates)
 
 # Opening  png device
 png("plot2.png", width=400, height=400)
 
 # Plotting the data
-plot(df$DateTime, data$Global_active_power,
-     type="l",
-     xlab="",
-     ylab="Global Active Power (kilowatts)")
-
+plot(df$DateTime, df$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 
 # Closing the device
 dev.off()
